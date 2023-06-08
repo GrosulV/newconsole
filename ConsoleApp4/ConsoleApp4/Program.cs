ea522d42b5b96e889cce6ce4c5662401e4b44cb2
GrosulV <127082028+GrosulV@users.noreply.github.com>
2023-06-08 12:47:35 +0300

    Add files via upload

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,376 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача №1
+            //Console.Write("x=");
+            //int x = int.Parse(Console.ReadLine());
+            //Console.Write("y=");
+            //int y = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"{x}+{y}={x + y}");
+
+            //Задача №2
+            //Console.Write("x=");
+            //int x = int.Parse(Console.ReadLine());
+            //Console.Write("y=");
+            //int y = int.Parse(Console.ReadLine());
+            //Console.Write("z=");
+            //int z = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"{x}+{y}+{z}={x + y+ z}");
+
+            //Задача №3
+            //Console.Write("x=");
+            //double x = double.Parse(Console.ReadLine());
+            //Console.Write("y=");
+            //double y = double.Parse(Console.ReadLine());
+            //Console.WriteLine($"{x}*{y}={x * y}");
+
+            //Задача №4
+            //Console.Write("x=");
+            //double x = double.Parse(Console.ReadLine());
+            //Console.Write("y=");
+            //double y = double.Parse(Console.ReadLine());
+            //Console.WriteLine($"{x}/{y}={x / y:f1}");
+
+            //Задача №5
+            //Console.Write("a=");
+            //int x = int.Parse(Console.ReadLine());
+            //Console.Write("b=");
+            //int y = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"{x}+{y}={y}+{x}");
+
+            //Задача №6
+            //Console.Write("Как тебя зовут?");
+            //string x = (Console.ReadLine());
+            //Console.Write("Сколько тебе лет?");
+            //int y = int.Parse (Console.ReadLine());
+            //Console.WriteLine($"{x},ты родился в {2023-y}");
+
+            //Задача №7
+            //Console.Write("Номинал купюры=");
+            //int x = int.Parse (Console.ReadLine());
+            //Console.Write("Колличество купюр=");
+            //int y = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"Сумма денег = {x*y:c}");
+
+            //Задача №8
+            //Console.Write("Длина=");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"p={4*a}");
+
+            //Задача №9
+            //Console.Write("Длина первой стороны=");
+            //int x = Convert.ToInt32 (Console.ReadLine());
+            //Console.Write("Длина второй стороны=");
+            //int y = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine($"p={2 * (x+y)}");
+
+            //Задача №10
+            //Console.Write("R=");
+            //int r = Convert.ToInt32(Console.ReadLine());
+            //const double pi = 3.14;
+            //Console.WriteLine($"{2 * pi * r}");
+
+            //Задача №11
+            //Console.Write("x=");
+            //int x = int.Parse(Console.ReadLine());
+            //if (x>0)
+            //{
+            //    Console.WriteLine($"{x+1}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"{x - 2}");
+            //}
+
+            //Задача №12
+            //Console.Write("x=");
+            //int x = int.Parse(Console.ReadLine());
+            //if (x > 0)
+            //{
+            //    Console.WriteLine($"{x + 1}");
+            //}
+            //if (x<0)
+            //{
+            //    Console.WriteLine($"{x - 2}");
+            //}
+            //if (x==0)
+            //{
+            //    Console.WriteLine($"Ответ= {10}");
+            //}
+
+            //Задача №13
+            //Console.Write("x=");
+            //int x = int.Parse(Console.ReadLine());
+            //if (x > 0)
+            //{
+            //    Console.WriteLine($"Число {x} положительное");
+            //}
+            //if (x < 0)
+            //{
+            //    Console.WriteLine($"Число {x} отрицательное");
+            //}
+            //if (x == 0)
+            //{
+            //    Console.WriteLine($"Число {x} нулевое");
+            //}
+
+            //Задача №14
+            //Console.Write("a=");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("b=");
+            //int b = int.Parse(Console.ReadLine());
+            //Console.Write("c=");
+            //int c = int.Parse(Console.ReadLine());
+            //if (a==b && b==c)
+            //{
+            //    Console.WriteLine($"Треугольник равностороний");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Треугольник неравностороний");
+            //}
+
+            //Задача №15
+            //Console.Write("x=");
+            //int x = int.Parse(Console.ReadLine());
+            //if (3%x==0)
+            //{
+            //    Console.WriteLine($"Число является делителем");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Число не является делителем");
+            //}
+
+            //Задача №16
+            //Random x = new Random();
+            //for (int a=1;a<=10;a++)
+            //{
+            //    int element = x.Next(-30, 30);
+            //    if (element > 0)
+            //    {
+            //        Console.WriteLine($"{element} - положительное");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"{element} - отрицательное");
+            //    }
+            //}
+
+            //Задача №17
+            //Random x = new Random();
+            //for (int a = 1; a <= 8; a++)
+            //{
+            //    int element = x.Next(-20, 330);
+            //    if (element > 0)
+            //    {
+            //        Console.WriteLine($"{element} - четное");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"{element} - нечетное");
+            //    }
+            //}
+
+            //Задача №18
+            //Console.Write("a=");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("b=");
+            //int b = int.Parse(Console.ReadLine());
+            //int max = Math.Max(a, b);
+            //    Console.WriteLine($"{max} максимальное");
+
+            //Задача №19
+            //Console.Write("День недели");
+            //int a = int.Parse(Console.ReadLine());
+
+            //switch (a)
+            //{
+            //    case 1:
+            //        Console.WriteLine($"Понедельник");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine($"Вторник");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine($"Среда");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine($"Четверг");
+            //        break;
+            //    case 5:
+            //        Console.WriteLine($"Пятница");
+            //        break;
+            //    case 6:
+            //        Console.WriteLine($"Суббота");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine($"Воскресенье");
+            //        break;
+            //    default: Console.WriteLine($"error");break;
+            //}
+
+            //Задача №20
+            //Console.Write("x=");
+            //for (int x = 10; x <= 99; x++)
+            //    if (x%2==0)
+            //    {
+            //        Console.WriteLine($"{x} - четное двухзначное");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"{x} - четное не двухзначное");
+            //    }
+
+            //Задача №26
+            //Console.Write("x=");
+            //for (int x = 10; x >= 1; x--)
+            //{
+            //        Console.WriteLine($"{x}");
+            //}
+
+            //Задача №25
+            //Console.Write("x=");
+            //for (int x = 1; x <= 18; x++)
+            //{
+            //    Console.Write($"{x}");
+            //}
+
+            //Задача №27
+            //Console.Write("x=");
+            //for (int x = 1; x <= 20; x+=2)
+            //{
+            //    Console.WriteLine($"x={x}\ty={5*x-2}");
+            //}
+
+            //Задача №28
+            //Console.Write("x=");
+            //for (int x = 1; x <=11; x += 2)
+            //{
+            //    if (x != 3)
+            //        Console.Write($"{x}\t");
+            //}
+
+
+            //Задача №29
+            //Console.Write("x=");
+            //for (int x =100; x<=200; x++)
+            //{
+            //    if(x%3==0)
+            //    Console.WriteLine($"{x}\t");
+            //}
+
+            //Задача №30
+            //Console.Write("x=");
+            //for (int x = 1; x <= 20; x++)
+            //{
+            //        Console.WriteLine($"{x} - штук {20.4*x:f2} руб.");
+            //}
+
+            //Задача №31
+            //Console.Write("x=");
+            //int[] omas = new int[6];
+            //for (int i = 0; i<omas.Length;i++)
+            //{
+            //    Console.Write($"omas [{i}]=");
+            //    omas[i] = int.Parse(Console.ReadLine());
+            //}
+            //for (int i = 0; i < omas.Length; i++)
+            //    Console.Write($"omas [{i}]={omas[i]}");
+
+            //Задача №32
+            //Console.Write("x=");
+            //int[] omas = new int[6];
+            //for (int i = 0; i < omas.Length; i++)
+            //{
+            //    Console.Write($"omas [{i}]=");
+            //    omas[i] = int.Parse(Console.ReadLine());
+            //}
+            //for (int i = 0; i < omas.Length; i++)
+            //    foreach (int el in omas)
+            //        Console.WriteLine($"{el}");
+
+            //Задача №33
+            //int[] omas = new int[12];
+            //Random rnd = new Random();
+            //for (int i = 0; i< omas.Length; i++)
+            //{
+            //    omas[i]= rnd.Next(163, 190);
+            //}
+            //foreach (int el in omas)
+            //    Console.WriteLine($"{el}");
+
+            //Задача №34
+            //double[] omas = new double[12];
+            //Random rnd = new Random();
+            //for (int i = 0; i < omas.Length; i++)
+            //{
+            //    omas[i] = -50 + 100 * rnd.NextDouble();
+            //}
+            //foreach (int el in omas)
+            //    Console.WriteLine($"{omas [0]}\t{omas [11]}");
+
+            ////Задача №35
+            //int[] omas = new int[] { 37, 0, 50, 46, 34, 46, 0, 13 };
+            //foreach (int el in omas)
+            //    Console.WriteLine($"{el}");
+
+            //Задача №36
+            //int[] omas = new int[] { -3, 5, 1, 11, -8, 4};
+            //for (int i = 5; i >=0; i--)
+            //{
+            //    Console.WriteLine($"{omas[i]}");
+            //}
+
+            //Задача №37
+            //int sum = 0;
+            //Random rnd = new Random();
+            //int[] omas = new int[31];
+            //for (int i = 0; i <= omas.Length-1; i++)
+            //{
+            //    omas[i] = rnd.Next(5, 60);
+            //    Console.WriteLine($"omas[{i+1}]={omas[i]}");
+            //}
+            //for (int i = 0; i <= omas.Length-1; i++)
+            //{
+            //    sum += omas[i];
+            //}
+            //Console.WriteLine($"Общее кол-во: {sum}");
+
+            //Задача №38
+            //double sum = 0;
+            //Random rnd = new Random();
+            //double[] omas = new double[20];
+            //for (int i = 0; i <= omas.Length - 1; i++)
+            //{
+            //    omas[i] = 50 + 50 * rnd.NextDouble();
+            //    Console.WriteLine($"omas[{i+1}]={omas[i]}");
+            //}
+            //for (int i = 0; i <= omas.Length - 1; i++)
+            //{
+            //    sum += omas[i];
+            //}
+            //Console.WriteLine($"Средний вес={sum / 20:f2}");
+
+            //Задача №39
+            //int count = 0;
+            //Random rnd = new Random();
+            //int[] omas = new int[8] { -37, 0, 50, 46, 34, 46, 4, 1 };
+            //foreach (int el in omas)
+            //{
+            //    if (el< 0)
+            //        count++;
+            //}
+            //Console.WriteLine($"Кол-во отриц. эл-ов = {count}");
+
+            //Задача №40
+
+                Console.Read();
+        }
+    }
+}
